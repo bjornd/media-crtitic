c6a066aa29e9add12786486d9753303e2f593a05
bjornd <echo.bjornd@gmail.com>
2013-08-17 00:26:39 +0300

    Fixed layout

--- www/app/view/Article.js
+++ www/app/view/Article.js
@@ -1,21 +1,44 @@
 Ext.define('MC.view.Article', {
     extend: 'Ext.Panel',
+    requires: [
+        'Ext.Img'
+    ],
     config: {
+        cls: 'x-article',
         padding: 10,
         scrollable: true,
         items: [{
-            itemId: 'articleAttributes',
-            tpl: new Ext.XTemplate(
-                '<div>{score}</div>',
-                '<img src="{image_url}" width="{image_width}" height="{image_height}"/>',
-                '<h3>{title} <small>{platform}</small></h3>',
-                '<ul>',
-                    '<li>Release date: {release_date}</li>',
-                    '<li>Rating: {maturity_rating}</li>',
-                    '<li>Publisher: {publisher}</li>',
-                '</ul>',
-                '<a href="http://www.metacritic.com{metacritic_url}">More info</a>'
-            )
+            xtype: 'container',
+            layout: 'hbox',
+            items: [{
+                itemId: 'articleImage',
+                padding: '0 10 0 0',
+                tpl: new Ext.XTemplate(
+                    '<img src="{image_url}" width="{image_width}" height="{image_height}"/>'
+                )
+            },{
+                itemId: 'articleAttributes',
+                tpl: new Ext.XTemplate(
+                    '<div class="x-artice-score">',
+                        '<div class="x-artice-score-points">',
+                            '<div class="x-artice-score-points-value">{score}</div>',
+                            '<div class="x-artice-score-points-max">out of 100</div>',
+                        '</div>',
+                        '<div class="x-artice-score-title">',
+                            '<a href="http://www.metacritic.com/about-metascores" target="_blank">metascore</a>',
+                        '</div>',
+                    '</div>',
+                    '<h3>{title} <small></small></h3>',
+                    '<ul>',
+                        '<li>For <b>{platform}</b></li>',
+                        '<li>Released on <b>{release_date}</b> by <b>{publisher}</b></li>',
+                        '<li>Rating: <b>{maturity_rating}</b></li>',
+                    '</ul>',
+                    '<a href="http://www.metacritic.com{metacritic_url}" target="_blank">More info on metacritic.com</a>'
+                ),
+                flex: 1
+            }],
+            padding: '0 0 10 0'
         },{
             xtype: 'button',
             itemId: 'articleBuyButton',
@@ -24,7 +47,10 @@ Ext.define('MC.view.Article', {
     },
 
     setData: function(data){
-        this.child('#articleAttributes').setData(data);
+        var imageCmp = this.down('#articleImage');
+
+        this.down('#articleAttributes').setData(data);
+        this.down('#articleImage').setData(data);
         this.child('#articleBuyButton').setText('Buy from Amazon for '+data.amazon_price);
     }
 });
