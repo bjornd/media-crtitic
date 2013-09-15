a9e766bafb49fb714fbf375589b79a74bbdfcaac
bjornd <echo.bjornd@gmail.com>
2013-09-15 18:54:07 +0300

    Removed unnecessary code from stores

--- www/app/store/EbayOffers.js
+++ www/app/store/EbayOffers.js
@@ -7,26 +7,6 @@ Ext.define('MC.store.EbayOffers', {
             url : 'http://192.168.0.13/api/games/:game_id/offers/ebay',
             noCache: false,
             enablePagingParams: false
-        },
-        listeners: {
-            beforeload: function(store, operation){
-                var params = operation.getParams(),
-                    key,
-                    url;
-
-                if (!store.getProxy().urlPattern) {
-                    store.getProxy().urlPattern = store.getProxy().getUrl();
-                }
-                url = store.getProxy().urlPattern;
-
-                for (key in params) {
-                    if (url.indexOf(':'+key) > -1) {
-                        url = url.replace(':'+key, params[key]);
-                        delete params[key];
-                    }
-                }
-                store.getProxy().setUrl(url);
-            }
         }
     }
 });
