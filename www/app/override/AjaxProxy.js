8d421b7aea99b8f2efbfe88d394c3bca0fdea04c
bjornd <echo.bjornd@gmail.com>
2013-09-19 10:06:55 +0300

    Layout fixes

--- www/app/override/AjaxProxy.js
+++ www/app/override/AjaxProxy.js
@@ -6,12 +6,16 @@ Ext.define('MC.override.AjaxProxy', {
         if (!window.debugMode) {
             baseUrl = 'http://media-critic-server.owl-hollow.net'
         } else if (window.cordova) {
-            baseUrl = 'http://192.168.0.13';
+            baseUrl = 'http://192.168.0.12';
         } else {
             baseUrl = '';
         }
 
-        this.callParent([baseUrl + url]);
+        if (url.indexOf(baseUrl) !== 0) {
+            url = baseUrl + url;
+        }
+
+        this.callParent([url]);
     }
 });
 
